32198b91b4f21d65aa8aa7a92ca5db67aff9c763
shelina98 <shelaveliu1@gmail.com>
2022-10-01 13:06:34 +0200

    View of product list DONE!

--- src/app/data.services.ts
+++ src/app/data.services.ts
@@ -11,27 +11,35 @@ export class DataService implements InMemoryDbService {
     const products = [
       {
         id: 1,
-        title: 'Seaman Cap',
+        title: 'Broccoli',
         description: 'Lorem ipsum . Voluptatem excepturi magnam nostrum dolore recusandae',
         price: '$40',
         amount:10,
-        imgUrl:"assets/logo.jpeg"
+        imgUrl:"assets/product/broccoli.png"
       },
       {
         id: 2,
-        title: 'T-shirt',
+        title: 'Cabbage',
         description: 'amet consectetur adipisicing elit.Lorem ipsum dolor sit ',
         price: '$80',
         amount:10,
-        imgUrl:"assets/logo.jpeg"
+        imgUrl:"assets/product/cabbage.png"
       },
       {
         id: 3,
-        title: 'Back Pack',
+        title: 'Onion',
         description: 'Voluptatem excepturi harum rerum aliquam magnam nostrum dolore recusandae',
         price: '$30',
         amount:10,
-        imgUrl:"assets/logo.jpeg"
+        imgUrl:"assets/product/onion.png"
+      },
+      {
+        id: 3,
+        title: 'Olives',
+        description: 'Voluptatem excepturi harum rerum aliquam magnam nostrum dolore recusandae',
+        price: '$30',
+        amount:10,
+        imgUrl:"assets/product/olives.png"
       }
     ];
     return { products };
